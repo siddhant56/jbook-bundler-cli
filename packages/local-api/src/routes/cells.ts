d648f84a80d86a6c27806599270f6e51dc34738e
Siddhant Trivedi <siddhanttrivedi56@gmail.com>
2024-01-08 23:13:00 +0530

    Fix local api error

--- packages/local-api/src/routes/cells.ts
+++ packages/local-api/src/routes/cells.ts
@@ -8,44 +8,44 @@ interface Cell {
   type: "text" | "code";
 }
 
+interface LocalApiError {
+  code: string;
+}
+
 export const createCellsRouter = (filename: string, dir: string) => {
   const router = express.Router();
-
   router.use(express.json());
 
   const fullPath = path.join(dir, filename);
 
   router.get("/cells", async (req, res) => {
-    //Make sure the cell storage file exists
-    //If it does not exist add in a default list of cells
-    //Read file
-    //Parse list of cells
-    //Send list of cella back to browser
+    const isLocalApiError = (err: any): err is LocalApiError => {
+      return typeof err.code === "string";
+    };
 
     try {
-      //Read file
+      // Read the file
       const result = await fs.readFile(fullPath, { encoding: "utf-8" });
 
-      console.log("Result ", result);
-
-      res.send(JSON.parse(JSON.stringify(result)));
-    } catch (error: any) {
-      if (error.code === "ENOENT") {
-        //Add code to create a file and add default cells
-        await fs.writeFile(fullPath, "[]", "utf-8");
-        res.send([]);
+      res.send(JSON.parse(result));
+    } catch (err) {
+      if (isLocalApiError(err)) {
+        if (err.code === "ENOENT") {
+          await fs.writeFile(fullPath, "[]", "utf-8");
+          res.send([]);
+        }
       } else {
-        throw error;
+        throw err;
       }
     }
   });
 
   router.post("/cells", async (req, res) => {
-    //Take list of cells from request object
-    //Serialize them
+    // Take the list of cells from the request obj
+    // serialize them
     const { cells }: { cells: Cell[] } = req.body;
-    //Write the cells into the file
 
+    // Write the cells into the file
     await fs.writeFile(fullPath, JSON.stringify(cells), "utf-8");
 
     res.send({ status: "ok" });
